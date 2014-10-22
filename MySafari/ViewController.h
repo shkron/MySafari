61077957c435b853ce5b61bc504ff18076021aa4
Alexey Emelyanov <emelyanoff@gmail.com>
2014-10-22 14:19:59 -0700

    Initial Commit

---
+++ MySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySafari
+//
+//  Created by Alex on 10/22/14.
+//  Copyright (c) 2014 Alexey Emelyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
