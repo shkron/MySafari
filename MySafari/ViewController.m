6ae5a39d3af98df6c469ed56460218736a9c573f
Alexey Emelyanov <emelyanoff@gmail.com>
2014-10-22 18:08:57 -0700

    TextField animation

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -8,11 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate>
+@interface ViewController () <UIWebViewDelegate, UITextFieldDelegate, UIScrollViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UIWebView *webView;
 @property (weak, nonatomic) IBOutlet UITextField *urlTextField;
 @property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+@property (weak, nonatomic) IBOutlet UILabel *urlTitle;
 
 @end
 
@@ -20,7 +21,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    self.webView.scrollView.delegate = self;
+
 }
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+
+    [UIView animateWithDuration:.5 animations:^{
+        self.urlTextField.alpha = 1;
+        self.urlTextField.frame = CGRectMake(self.urlTextField.frame.origin.x, -40, self.urlTextField.frame.size.width, self.urlTextField.frame.size.height);
+    }];
+
+//    if(scrollView.contentOffset.y <= 0) {
+//        self.urlTextField.alpha = 1;
+//        [UIView beginAnimations:nil context:NULL];
+//        [UIView setAnimationDelegate:self];
+//        [UIView setAnimationDuration:0.5];
+//        [UIView setAnimationBeginsFromCurrentState:YES];
+//        self.urlTextField.frame = CGRectMake(self.urlTextField.frame.origin.x, self.urlTextField.frame.origin.y, self.urlTextField.frame.size.width, self.urlTextField.frame.size.height);
+//        [UIView commitAnimations];
+//    } else {
+//
+//        self.urlTextField.alpha = 0.2;
+//        [UIView beginAnimations:nil context:NULL];
+//        [UIView setAnimationDelegate:self];
+//        [UIView setAnimationDuration:0.5];
+//        [UIView setAnimationBeginsFromCurrentState:YES];
+//       self.urlTextField.frame = CGRectMake(self.urlTextField.frame.origin.x, -30, self.urlTextField.frame.size.width, self.urlTextField.frame.size.height);
+//        [UIView commitAnimations];
+//    }
+}
+
 
 -(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
@@ -38,6 +70,8 @@
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [self.webView loadRequest:request];
 
+
+
     return YES;
 }
 
@@ -45,11 +79,14 @@
 {
     [self.activityIndicator startAnimating];
 
+
 }
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView
 {
     [self.activityIndicator stopAnimating];
+    self.urlTitle.text = ([self.webView stringByEvaluatingJavaScriptFromString:@"document.title"]);
+
 }
 - (IBAction)onBackButtonPressed:(id)sender {
 
@@ -62,10 +99,22 @@
 
 
 }
+
+- (IBAction)onComingSoonButtonPressed:(id)sender {
+    UIAlertController *alert =[UIAlertController alertControllerWithTitle:@"Welcome" message:@"Coming Soon!" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"ok" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:ok];
+    [self presentViewController:alert animated:YES completion:nil];
+
+}
+
 - (IBAction)onReloadButtonPressed:(id)sender {
     [self.webView reload];
 }
 
+- (IBAction)onClearButtonPressed:(id)sender {
+    self.urlTextField.text = @"";
+}
 
 
 - (IBAction)onStopLoadingButtonPressed:(id)sender {
